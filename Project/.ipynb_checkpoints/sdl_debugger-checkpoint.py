35464b0b8cf5e0450652cbff19ebad98712c3dac
jacquiunciano <jacqueline.unciano@gmail.com>
2024-04-14 16:56:57 -0400

    loading insanity

--- Project/.ipynb_checkpoints/sdl_debugger-checkpoint.py
+++ Project/.ipynb_checkpoints/sdl_debugger-checkpoint.py
@@ -1,17 +1,5 @@
 #!/usr/bin/env python3
 
-import pandas as pd
-import numpy as np
-import time
-from pyspark.sql.functions import when, rand
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType
-
-from sparknlp.base import DocumentAssembler, Pipeline
-from sparknlp.annotator import (
-    UniversalSentenceEncoder,
-    SentimentDLApproach
-)
-
 import torch
 
 if torch.cuda.is_available():
@@ -20,41 +8,31 @@ if torch.cuda.is_available():
     print("Number of GPUs available: ", torch.cuda.device_count())
 else:
     print("CUDA is not available. Using CPU instead.")
+    
+import pandas as pd
+import numpy as np
+import time
+import os
+from pyspark.sql.functions import when
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
 import sparknlp
-import findspark as fs
-fs.init('/home/jdu5sq/spark-3.4.1-bin-hadoop3')
-fs.find()
+from sparknlp.base import DocumentAssembler, Pipeline
+from sparknlp.annotator import (
+    UniversalSentenceEncoder,
+    SentimentDLApproach
+)
 
-print("Import done!")
-data_path = "/home/jdu5sq/Documents/MSDS/DS5110/Project/"
+os.system("wget -N https://s3.amazonaws.com/auxdata.johnsnowlabs.com/public/resources/en/spell/words.txt -P /tmp")
+os.system("rm -rf /tmp/sentiment.parquet")
+os.system("wget -N https://s3.amazonaws.com/auxdata.johnsnowlabs.com/public/resources/en/sentiment.parquet.zip -P /tmp")
+os.system("unzip /tmp/sentiment.parquet.zip -d /tmp/")
 
-from pyspark.sql import SparkSession
+print("Import and downloads finished.")
 
-def start_spark_session():
-    spark = SparkSession.builder \
-        .appName("GPU Spark NLP") \
-        .master("local[10]") \
-        .config("spark.driver.memory", "16G") \
-        .config("spark.executor.memory", "12G") \
-        .config("spark.executor.instances", "4") \
-        .config("spark.task.cpus", "1") \
-        .config("spark.task.resource.gpu.amount", "0.5") \
-        .config("spark.executor.cores", "2") \
-        .config("spark.executor.resource.gpu.amount", "1") \
-        .config("spark.executor.resource.gpu.discoveryScript", data_path+"/getGpusResources.sh") \
-        .config("spark.driver.resource.gpu.amount", "1") \
-        .config("spark.driver.resource.gpu.discoveryScript", data_path+"/getGpusResources.sh") \
-        .config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp_2.12:4.2.0,com.johnsnowlabs.nlp:spark-nlp-gpu_2.12:4.2.0") \
-        .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer") \
-        .config("spark.kryoserializer.buffer.max", "2000M") \
-        .config("spark.driver.maxResultSize", "0") \
-        .getOrCreate()
-    return spark
+spark = sparknlp.start(gpu=True)
 
-spark = start_spark_session()
-
-print("Spark started.")
+print("Spark initialised.")
 
 print("Starting dataset making...")
 
@@ -67,37 +45,33 @@ schema = StructType([
 trainDataset = spark.read \
     .option("header", False) \
     .schema(schema) \
-    .csv(data_path+"debugger_train.csv")
+    .csv("/home/jdu5sq/Documents/MSDS/DS5110/Project/debugger_train.csv")
+
+print("Finished getting dataset.")
 
 debugDataset = trainDataset.withColumn("label", when(trainDataset["label"] == 2, 1).otherwise(0))
 
-print("Data loaded.")
-
-# Start the timer
-start_time = time.time()
+spark.sparkContext.setLogLevel("ERROR")
 
 documentAssembler = DocumentAssembler() \
     .setInputCol("text") \
     .setOutputCol("document")
 print("documentAssembler finished!")
-da_time = time.time() - start_time
 
 useEmbeddings = UniversalSentenceEncoder.pretrained() \
     .setInputCols(["document"]) \
     .setOutputCol("sentence_embeddings")
 print("useEmbeddings finished!")
-ue_time = time.time() - da_time
 
 sentimentdl = SentimentDLApproach() \
     .setInputCols(["sentence_embeddings"]) \
     .setOutputCol("sentiment") \
     .setLabelColumn("label") \
-    .setbatchSize(32) \
-    .setlr(1e-3) \
+    .setBatchSize(32) \
+    .setLr(1e-3) \
     .setMaxEpochs(5) \
     .setEnableOutputLogs(True)
 print("sentimentdl finished!")
-dla_time = time.time() - ue_time
 
 pipeline = Pipeline() \
     .setStages(
@@ -109,7 +83,9 @@ pipeline = Pipeline() \
     )
 
 print("Pipeline finished!")
-pipe_time = time.time() - dla_time
+
+# Start the timer
+start_time = time.time()
 
 pipelineModel = pipeline.fit(debugDataset)
 
@@ -120,20 +96,14 @@ end_time = time.time()
 
 # Calculate the total time taken
 total_time = end_time - start_time
-print(f"Total DocumentAssembler time: {da_time} seconds")
-print(f"Total UniversalSentenceEncoder time: {ue_time} seconds")
-print(f"Total SentimentDLApproach time: {dla_time} seconds")
-print(f"Total Pipline time: {pip_time} seconds")
 print(f"Total execution time: {total_time} seconds")
 
 # cat ~/annotator_logs/SentimentDLApproach_12faa854e3b3.log
 
 print("Starting logs.")
 
-# Define the log file path
-log_file_path = '~/annotator_logs/SentimentDLApproach_12faa854e3b3.log'
+os.system('cat ~/annotator_logs/SentimentDLApproach_12faa854e3b3.log')
 
-# Print the contents of the log file
-print("Contents of the log file:")
-with open(log_file_path, 'r') as log_file:
-    print(log_file.read())
+print("Logs finished.")
+
+spark.stop()
--- Project/my_slurm.sh
+++ Project/my_slurm.sh
@@ -3,14 +3,13 @@
 #SBATCH -A ds_6050
 #SBATCH -p gpu                               # Specify the GPU partition
 #SBATCH --gres=gpu:rtx3090:2                 # Request 2 RTX 3090 GPUs
-#SBATCH --mem=16G                            # Request 16GB memory per GPU
-#SBATCH -c 2                                 # Request 2 cores per GPU
+#SBATCH -c 4                                 # Request 2 cores per GPU
 #SBATCH -t 00:30:00                          # Set a time limit of 30 minutes
 #SBATCH -J data_sys                          # Job name
 #SBATCH -o data_sys-%A.out                   # Standard output file
 #SBATCH -e data_sys-%A.err                   # Standard error file
 
-# ijob -A ds_6050 -p gpu --gres=gpu:rtx3090:4 -c 10 -t 00:30:00
+# ijob -A ds_6050 -p gpu --gres=gpu:rtx3090:2 -c 4 -t 00:30:00
 module purge
 # Load modules
 module load apptainer/1.2.2 pytorch/2.0.1 java/11 gcc/11.4.0 openmpi/4.1.4 python/3.11.4 spark/3.4.1
